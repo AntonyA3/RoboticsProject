ced206782e7f23d9ab07922bbbe0ac7cd6f6bc54
AntonyA3 <antonyademefun@gmail.com>
2020-12-15 20:36:35 +0000

    Attempt at robot controller

--- scripts/robotcontrol_node.py
+++ scripts/robotcontrol_node.py
@@ -7,7 +7,7 @@ import math
 import numpy as np
 
 from sensor_msgs.msg import (Temperature, LaserScan)
-from geometry_msgs.msg import (Pose, PoseWithCovarianceStamped,PoseWithCovariance, Quaternion, Transform, TransformStamped, Twist)
+from geometry_msgs.msg import (Pose,PoseStamped, PoseWithCovarianceStamped,PoseWithCovariance, Quaternion, Transform, TransformStamped, Twist)
 from nav_msgs.msg import (OccupancyGrid, Path, Odometry)
 from std_msgs.msg import Int8MultiArray
 from tf import (TransformBroadcaster)
@@ -15,13 +15,18 @@ from tf.msg import tfMessage
 
 class RobotController(object):
     def __init__(self):
+        self.TO_TARGET = 0
+        self.GOING_AROUND_OBSTACLE = 1
+        self.TRYING_TO_ESCAPE = 2
+        self.TARGET_REACHED = 3
+
         self.noPath = False
         self.temperatures = [0,0,0,0]
         self.estimated_position = [0,0]
         self.estimated_angle = 0
         self.map = OccupancyGrid()
         self.target_position = [10,10]
-
+        self.navigation_mode = self.TARGET_REACHED
         forward_action = Twist();
         forward_action.linear.x = 5;
 
@@ -57,15 +62,18 @@ class RobotController(object):
 
         def update_pose(data):
             self.estimated_position = [data.pose.pose.position.x, data.pose.pose.position.y]
-
             self.estimated_angle = util.getHeading(data.pose.pose.orientation)
 
+        def update_goal(data):
+            self.target_position[0] = data.pose.position.x
+            self.target_position[1] = data.pose.position.y
+            self.navigation_mode = self.TO_TARGET
 
         self.temperatureSubscriber = rospy.Subscriber("temperatures", Int8MultiArray, update_temperature)
         self.mapSubscriber = rospy.Subscriber("map", OccupancyGrid, update_map)
         self.movementPubliser = rospy.Publisher("cmd_vel", Twist, queue_size=10)
         self.amclPoseSubscriber = rospy.Subscriber("amcl_pose",PoseWithCovarianceStamped, update_pose)
-
+        self.goalSubscriber = rospy.Subscriber("/move_base_simple/goal", PoseStamped, update_goal)
 
 
 
@@ -80,39 +88,114 @@ class RobotController(object):
         front_blocked = False
         left_blocked  = False
         right_blocked = False
-        for i in range(30):
-            c = np.add(cell, np.multiply(forwardVector, i))
-            c[0] = int(c[0])
-            c[1] = int(c[1])
-            cflat = int(c[0] * self.map.info.width + c[1])
+        for i in range(20):
+            for j in range(-10, 10, 1):
+                c = np.add(cell,np.add(np.multiply(forwardVector, i), np.multiply(rightVector, j)))
+                c[0] = int(c[0])
+                c[1] = int(c[1])
+                cflat = int(c[0] * self.map.info.width + c[1])
 
-            if(cflat >= 0 and cflat < self.map.info.width * self.map.info.height ):
-                if(self.map.data[cflat] > 40):
-                    front_blocked = True
+                if(cflat >= 0 and cflat < self.map.info.width * self.map.info.height ):
+                    if(self.map.data[cflat] > 40):
+                        front_blocked = True
 
-        if not front_blocked:
-            self.movementPubliser.publish(self.actions["forward"])
-            print("front not blocked")
-        else:
-            self.movementPubliser.publish(self.actions["left"])
+                c = np.add(cell,np.add(np.multiply(leftVector, i), np.multiply(forwardVector, j)))
+                c[0] = int(c[0])
+                c[1] = int(c[1])
+                cflat = int(c[0] * self.map.info.width + c[1])
 
-            print("front blocked")
-
-
-            """
-            c = np.add(cell, np.multiply(leftVector, i))
-            if(cflat >= 0 and cflat < self.map.info.width * self.map.info.height ):
-                if(self.map.data[cflat] > 40):
-                    left_blocked = True
-                    print("left blocked")
+                if(cflat >= 0 and cflat < self.map.info.width * self.map.info.height ):
+                    if(self.map.data[cflat] > 40):
+                        left_blocked = True
 
 
-            c = np.add(cell, np.multiply(rightVector, i))
-            if(cflat >= 0 and cflat < self.map.info.width * self.map.info.height ):
-                if(self.map.data[cflat] > 40):
-                    right_blocked = True
-                    print("right blocked")
-            """
+                c = np.add(cell,np.add(np.multiply(rightVector, i), np.multiply(forwardVector, j)))
+                c[0] = int(c[0])
+                c[1] = int(c[1])
+                cflat = int(c[0] * self.map.info.width + c[1])
+
+                if(cflat >= 0 and cflat < self.map.info.width * self.map.info.height ):
+                    if(self.map.data[cflat] > 40):
+                        right_blocked = True
+
+        if self.temperatures[0] > 50:
+            front_blocked = True
+        if self.temperatures[1] > 50:
+            left_blocked = True
+        if self.temperatures[2] > 50:
+            right_blocked = True
+
+        if self.navigation_mode is self.TO_TARGET:
+            delta_target = np.subtract(self.target_position, self.estimated_position)
+            if not front_blocked:
+                action = self.actions["forward"]
+                if(abs(delta_target[0]) > abs(delta_target[1])):
+                  self.movementPubliser.publish()
+
+                  if(self.target_position[1] > self.estimated_position[1]):
+                      action.angular.z = 0.2
+                  if(self.target_position[1] < self.estimated_position[1]):
+                    action.angular.z = -0.2
+
+                if(abs(delta_target[0]) <= abs(delta_target[1])):
+
+                    if(self.target_position[0] > self.estimated_position[0]):
+                      action.angular.z = 0.2
+                    if(self.target_position[0] < self.estimated_position[0]):
+                      action.angular.z = -0.2
+                self.movementPubliser.publish(action);
+
+                print("front not blocked")
+            else:
+                print("front is blocked")
+            if np.sqrt(np.dot(self.target_position, self.estimated_position) * np.dot(self.target_position, self.estimated_position)) < 20:
+                print("found")
+                self.navigation_mode = self.TARGET_REACHED
+
+
+        """
+        if self.navigation_mode is self.GOING_AROUND_OBSTACLE:
+            poseToTarget = np.subtract(self.target_position, self.estimated_position)
+            if front_blocked:
+                #decide which direction to turn
+                if(poseToTarget[0] > 0):
+                    self.movementPubliser.publish(self.actions["right"])
+                else:
+                    self.movementPubliser.publish(self.actions["left"])
+            else:
+                self.movementPubliser.publish(self.actions["forward"])
+                if(poseToTarget[0] > 0):
+                    if not right_blocked:
+                        self.movementPubliser.publish(self.actions["right"])
+                        self.navigation_mode = self.TO_TARGET
+                    if right_blocked and front_blocked:
+                        self.navigation_mode = self.TRYING_TO_ESCAPE
+                if(poseToTarget[0] < 0):
+                    if not left_blocked:
+                        self.movementPubliser.publish(self.actions["left"])
+                        self.navigation_mode = self.TO_TARGET
+                    if left_blocked and front_blocked:
+                        self.navigation_mode = self.TRYING_TO_ESCAPE
+        if self.navigation_mode is self.TRYING_TO_ESCAPE:
+
+        """
+
+
+
+        """
+        c = np.add(cell, np.multiply(leftVector, i))
+        if(cflat >= 0 and cflat < self.map.info.width * self.map.info.height ):
+            if(self.map.data[cflat] > 40):
+                left_blocked = True
+                print("left blocked")
+
+
+        c = np.add(cell, np.multiply(rightVector, i))
+        if(cflat >= 0 and cflat < self.map.info.width * self.map.info.height ):
+            if(self.map.data[cflat] > 40):
+                right_blocked = True
+                print("right blocked")
+        """
 
 
         """
